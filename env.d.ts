0805994477525ccbde3221720ee508404c2f8938
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2025-12-25 11:59:56 +0100

    feat: add env variables for API backend

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+  readonly NOKRONER_API_HOST: string
+  readonly NOKRONER_API_PORT: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
