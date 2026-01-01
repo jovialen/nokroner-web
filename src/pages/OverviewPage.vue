826b8e700ff8f6c566483c1aa96151f771571bcb
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2026-01-01 10:15:38 +0100

    feat: add dummy income/expenses chart

--- src/pages/OverviewPage.vue
+++ src/pages/OverviewPage.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
+import Card from '@/components/card/CardComponent.vue';
 import NumericStatCard from '@/components/card/NumericStatCard.vue'
 import StatBar from '@/components/card/StatBar.vue'
+import IncomeExpenseChart from '@/components/charts/IncomeExpenseChart.vue';
 import { useUserOwnerStore } from '@/stores/owners'
 
 const userOwner = useUserOwnerStore()
 </script>
 
 <template>
-  <h1 class="text-xl">{{ $t('page.overview.title') }}</h1>
-
   <StatBar v-if="userOwner.userOwner">
     <NumericStatCard :title="$t('stats.net_worth')" :number="userOwner.userOwner!.net_worth"
       :change="userOwner.userOwner!.net_worth / userOwner.userOwner!.net_worth_last_month" />
@@ -20,6 +20,17 @@ const userOwner = useUserOwnerStore()
       :number="userOwner.userOwner!.recent_income - userOwner.userOwner!.recent_expenses"
       :change="(userOwner.userOwner!.recent_income - userOwner.userOwner!.recent_expenses) / (userOwner.userOwner!.previous_income - userOwner.userOwner!.previous_expenses)" />
   </StatBar>
+
+  <Card class="px-6 py-4 space-y-2">
+    <div class="flex justify-between items-center">
+      <h2 class="text-xl">Pengeflyt</h2>
+      <select name="graphPeriod" id="graphPeriod" class="px-4 py-2 border border-border rounded-lg">
+        <option value="monthly">Per måned</option>
+        <option value="weekly">Per uke</option>
+      </select>
+    </div>
+    <IncomeExpenseChart />
+  </Card>
 </template>
 
 <style scoped></style>
