643838aa3eec71034a018a161f88a80ae0d9e87c
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2026-01-03 19:45:22 +0100

    feat: add day to period selector

--- src/components/charts/InputPeriodSelector.vue
+++ src/components/charts/InputPeriodSelector.vue
@@ -6,7 +6,8 @@ const model = defineModel()
 defineProps({
   includeYear: Boolean,
   includeMonth: Boolean,
-  includeWeek: Boolean
+  includeWeek: Boolean,
+  includeDay: Boolean,
 })
 </script>
 
@@ -14,7 +15,8 @@ defineProps({
   <InputSelector v-model="model" :options="[
     includeYear ? { label: $t('page.overview.per_period', { period: $t('schema.period.year') }), value: 'year' } : undefined,
     includeMonth ? { label: $t('page.overview.per_period', { period: $t('schema.period.month') }), value: 'month' } : undefined,
-    includeWeek ? { label: $t('page.overview.per_period', { period: $t('schema.period.week') }), value: 'week' } : undefined]
+    includeWeek ? { label: $t('page.overview.per_period', { period: $t('schema.period.week') }), value: 'week' } : undefined,
+    includeDay ? { label: $t('page.overview.per_period', { period: $t('schema.period.day') }), value: 'day' } : undefined]
     .filter(v => v != undefined)" label-hidden>
     {{ $t('schema.period.period') }}
   </InputSelector>
